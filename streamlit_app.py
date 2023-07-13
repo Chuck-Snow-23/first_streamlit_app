86437eafa0b4c8f6a1a10a982372d6381b58312d
Chuck-Snow-23 <138630730+Chuck-Snow-23@users.noreply.github.com>
2023-07-13 11:22:23 +1000

    adding try except

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,19 +27,21 @@ streamlit.dataframe(fruits_to_show)
 # New section to disaplay fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json())
-
-# normalise json 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output as table
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.erro()
+ 
 
 #dont run anything past here
-# streamlit.stop()
+streamlit.stop()
 
 #snowflake connector
 
